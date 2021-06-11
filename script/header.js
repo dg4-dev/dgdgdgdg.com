b3e7606aa9adec1110131b53599f0efeeef6ef3e
dg4-design <ren.y@dgdgdgdg.com>
2021-06-11 23:12:03 +0900

    create header

--- script/header.js
+++ script/header.js
@@ -1,2 +1,23 @@
-var html = `<header></header>`;
+var html = `
+<header class="is-flex center">
+  <a href="/">
+    <div class="logo">
+      <img src="/image/logo.svg">
+    </div>
+  </a>
+  <nav>
+    <ul class="is-flex">
+      <li>
+        <a href="/about/">About</a>
+      </li>
+      <li>
+        <a href="/works/">Works</a>
+      </li>
+      <li>
+        <a href="/contact/">Contact</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+`;
 document.write(html);
