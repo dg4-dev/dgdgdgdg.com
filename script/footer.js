9916997fb135ff89ec8c38329f755d194e5df03a
dg4-design <ren.y@dgdgdgdg.com>
2021-06-12 01:36:57 +0900

    create footer

--- script/footer.js
+++ script/footer.js
@@ -1,2 +1,32 @@
-var html = `<footer></footer>`;
+var html = `
+<footer>
+  <div class="container center is-flex">
+    <ul class="is-flex">
+      <li class="is-flex">
+        <a class="page-link" href="/about/">About</a>
+        <a href="/about/#overview">概要</a>
+        <a href="/about/#skill">スキル</a>
+        <a href="/about/#tool">ツール</a>
+      </li>
+      <li class="is-flex">
+        <a class="page-link" href="/works/">Works</a>
+        <a href="/works/#">制作物</a>
+        <a href=""></a>
+      </li>
+      <li class="is-flex">
+        <a class="page-link" href="/contact/">Contact</a>
+        <a href="/contact/#message">メッセージ</a>
+        <a href="/contact/#link">リンク</a>
+        <a href=""></a>
+      </li>
+    </ul>
+    <div class="credit">
+      <div class="logo-white">
+        <img src="/image/logo-white.svg">
+      </div>
+      <p>©︎ 2021 dgdgdgdg</p>
+    </div>
+  </div>
+</footer>
+`;
 document.write(html);
