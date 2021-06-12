7f31afc14c1c73bca98e1dbdb582d28cefd74769
dgdgdgdg <ren.y@dgdgdgdg.com>
2021-06-13 06:51:18 +0900

    update contact mail button

--- script/main.js
+++ script/main.js
@@ -4,3 +4,13 @@ const menu = document.getElementById("menu");
 button.addEventListener("click", function () {
   menu.classList.toggle("open");
 });
+
+const mailbutton = document.getElementById("sendmail");
+
+mailbutton.addEventListener("click", function () {
+  const result = window.confirm("メールを作成しますか？");
+
+  if (result) {
+    location.href = "mailto:info@dgdgdgdg.com";
+  }
+});
