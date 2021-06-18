d04f65408e256628442d0a38f5d2ddba60c96c4d
dgdgdgdg <ren.y@dgdgdgdg.com>
2021-06-18 22:19:18 +0900

    fix contact link

--- script/main.js
+++ script/main.js
@@ -1,16 +1,4 @@
 window.onload = function () {
-  const mailbutton = document.getElementById("sendmail");
-
-  mailbutton.addEventListener("click", function () {
-    const result = window.confirm("メールを作成しますか？");
-
-    if (result) {
-      location.href = "mailto:info@dgdgdgdg.com";
-    }
-  });
-};
-
-window.onload = function () {
   const button = document.getElementById("menuopen");
   const menu = document.getElementById("menu");
 
