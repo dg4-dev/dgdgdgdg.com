e6f24b6fea63b45c0bb206ed936fbd6f75461193
dgdgdgdg <ren.y@dgdgdgdg.com>
2021-06-18 17:59:59 +0900

    document.write() を廃止できたが、JSの読み込みがよくわからず

--- script/footer.js
+++ script/footer.js
@@ -1,32 +1,15 @@
-var html = `
-<footer>
-  <div class="container center is-flex">
-    <ul class="is-flex">
-      <li class="is-flex">
-        <a class="page-link" href="/about/">About</a>
-        <a href="/about/#overview">概要</a>
-        <a href="/about/#skill">スキル</a>
-        <a href="/about/#tool">ツール</a>
-      </li>
-      <li class="is-flex">
-        <a class="page-link" href="/works/">Works</a>
-        <a href="/works/#">制作物</a>
-        <a href=""></a>
-      </li>
-      <li class="is-flex">
-        <a class="page-link" href="/contact/">Contact</a>
-        <a href="/contact/#message">メッセージ</a>
-        <a href="/contact/#link">リンク</a>
-        <a href=""></a>
-      </li>
-    </ul>
-    <div class="credit">
-      <div class="logo-white">
-        <img src="/image/logo-white.svg">
-      </div>
-      <p>©︎ 2021 dgdgdgdg</p>
-    </div>
-  </div>
-</footer>
-`;
-document.write(html);
+const xfr = new XMLHttpRequest(),
+  Fmethod = "GET",
+  Furl = "/common/footer.html";
+const Fbox = document.getElementById("footer");
+
+xfr.responseType = "document";
+xfr.open(Fmethod, Furl, true);
+xfr.onreadystatechange = function () {
+  if (xfr.readyState === 4 && xfr.status === 200) {
+    const restxt = xfr.responseXML;
+    const int = restxt.getElementsByTagName("footer")[0];
+    Fbox.innerHTML = int.innerHTML;
+  }
+};
+xfr.send();
--- script/header.js
+++ script/header.js
@@ -1,26 +1,15 @@
-var html = `
-<header class="is-flex center">
-  <a class="totop" href="/">
-    <div class="logo">
-      <img src="/image/logo.svg">
-    </div>
-  </a>
-  <nav id="menu">
-    <ul class="is-flex">
-      <li>
-        <a href="/about/">About</a>
-      </li>
-      <li>
-        <a href="/works/">Works</a>
-      </li>
-      <li>
-        <a href="/contact/">Contact</a>
-      </li>
-    </ul>
-  </nav>
-  <div class="is-sp is-flex" id="menuopen">
-    <div class="hamburger center"></div>
-  </div>
-</header>
-`;
-document.write(html);
+const xhr = new XMLHttpRequest(),
+  Hmethod = "GET",
+  Hurl = "/common/header.html";
+const Hbox = document.getElementById("header");
+
+xhr.responseType = "document";
+xhr.open(Hmethod, Hurl, true);
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    const restxt = xhr.responseXML;
+    const int = restxt.getElementsByTagName("header")[0];
+    Hbox.innerHTML = int.innerHTML;
+  }
+};
+xhr.send();
