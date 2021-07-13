ce0716366d48f3032c16116984c36916502baf8d
dgdgdgdg <ren.y@dgdgdgdg.com>
2021-07-13 13:50:14 +0900

    update: contactのlinkをJSに置き換え

---
+++ script/contactLinks.js
@@ -0,0 +1,33 @@
+const linkData = [
+  { name: "Clubhouse" },
+  { name: "Qiita" },
+  { name: "Note" },
+  { name: "Twitter" },
+  { name: "Facebook" },
+  { name: "Behance" },
+  { name: "YouTube" },
+  { name: "Apple Music" },
+  { name: "Instagram" },
+  { name: "Gitlab" },
+  { name: "Github" },
+  { name: "Suzuri" },
+];
+
+const linkList = document.getElementById("linkList");
+
+for (i = 0; i < linkData.length; i++) {
+  const formated = linkData[i].name.toLowerCase().replace(/\s+/g, "");
+
+  linkList.insertAdjacentHTML(
+    "beforeend",
+    "<li><a href='https://" +
+      formated +
+      ".dgdgdgdg.com' target='_blank' rel='noopener norefferer' class='is-flex'><div class='icon'><img src='/image/i-" +
+      formated +
+      ".webp' alt='" +
+      formated +
+      " icon'></div><span class='sub'>" +
+      formated +
+      "</span></a></li>"
+  );
+}
