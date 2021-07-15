c2a07afb8bc4a95f29621b313ca3aa9a0d525433
dgdgdgdg <ren.y@dgdgdgdg.com>
2021-07-15 23:26:55 +0900

    update: ヘッダーのエラー取りました　えらい

--- script/main.js
+++ script/main.js
@@ -1,8 +1,1 @@
-window.onload = function () {
-  const button = document.getElementById("menuopen");
-  const menu = document.getElementById("menu");
 
-  button.addEventListener("click", function () {
-    menu.classList.toggle("open");
-  });
-};
