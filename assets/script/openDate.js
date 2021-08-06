e822526454656462c8666407c5c5622854e95b96
dgdgdgdg <ren.y@dgdgdgdg.com>
2021-08-06 15:37:37 +0900

    update: 開業日からの日数を計算

---
+++ assets/script/openDate.js
@@ -0,0 +1,8 @@
+const toToday = document.getElementById("toToday");
+const openDay = new Date(2021, 1, 1);
+var today = new Date();
+
+toToday.insertAdjacentHTML(
+  "beforeend",
+  "(今日で" + Math.ceil((today - openDay) / 86400000) + "日)"
+);
