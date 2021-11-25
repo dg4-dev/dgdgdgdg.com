b39bf7828ecfffb5827606bb017aa3dd00bef511
dgdgdgdg <ren.y@dgdgdgdg.com>
2021-11-25 16:11:52 +0900

    fix: toolsを最適化

--- assets/script/tools.js
+++ assets/script/tools.js
@@ -3,42 +3,34 @@ const toolList = [
     contentName: "Software",
     items: [
       {
-        image: "adobe-ai",
         name: "Adobe Illustrator",
         gen: "Latest version",
       },
       {
-        image: "adobe-xd",
         name: "Adobe XD",
         gen: "Latest version",
       },
       {
-        image: "adobe-pr",
         name: "Adobe Premiere Pro",
         gen: "Latest version",
       },
       {
-        image: "adobe-ae",
         name: "Adobe After Effects",
         gen: "Latest version",
       },
       {
-        image: "adobe-ps",
         name: "Adobe Photoshop",
         gen: "Latest version",
       },
       {
-        image: "adobe-lr",
         name: "Adobe Lightroom",
         gen: "Latest version",
       },
       {
-        image: "studio",
         name: "STUDIO",
         gen: "Latest version",
       },
       {
-        image: "figma",
         name: "Figma",
         gen: "Latest version",
       },
@@ -48,27 +40,22 @@ const toolList = [
     contentName: "Hardware",
     items: [
       {
-        image: "macbook",
         name: 'MacBook Pro 16"',
         gen: "2019",
       },
       {
-        image: "ipad",
         name: 'iPad Pro 12.9"',
         gen: "2020 (4th Gen)",
       },
       {
-        image: "ipadmini",
         name: "iPad mini",
         gen: "2021 (6th Gen)",
       },
       {
-        image: "iphone13pro",
         name: "iPhone 13 Pro",
         gen: "2021",
       },
       {
-        image: "iphone",
         name: "iPhone SE",
         gen: "2020 (2nd Gen)",
       },
@@ -78,32 +65,26 @@ const toolList = [
     contentName: "Shooting equipment",
     items: [
       {
-        image: "xs10",
         name: "FUJIFILM X-S10",
         gen: "2020",
       },
       {
-        image: "xf35",
         name: "XF 35mm F1.4 R",
         gen: "2012",
       },
       {
-        image: "xf1855",
         name: "XF 18-55mm F2.8-4 R OIS",
         gen: "2012",
       },
       {
-        image: "wtulens",
         name: "GIZMON Wtulens L",
         gen: "2018",
       },
       {
-        image: "rsc2",
         name: "DJI RSC 2",
         gen: "2020",
       },
       {
-        image: "om4",
         name: "DJI OM4",
         gen: "2020",
       },
@@ -121,9 +102,12 @@ toolList.forEach((value, index) => {
 
   value.items.forEach((valueChild) => {
     const toolContent = document.getElementById(`toolContent${index}`);
+    const imgName = valueChild.name.toLowerCase().replace(/[." -]/g, "");
+
+    console.log(imgName);
     toolContent.insertAdjacentHTML(
       "beforeend",
-      `<div class="item"><div class="item-img center"><img src="/assets/image/tool-${valueChild.image}.webp" /></div><p class="item-name center">${valueChild.name}</p><p class="item-gen center">${valueChild.gen}</p></div>`
+      `<div class="item"><div class="item-img center"><img src="/assets/image/tool-${imgName}.webp" /></div><p class="item-name center">${valueChild.name}</p><p class="item-gen center">${valueChild.gen}</p></div>`
     );
   });
 });
