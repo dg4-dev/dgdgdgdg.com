5f9e3a8029ad721a8b7950a940be0dffc206a6fd
dgdgdgdg <ren.y@dgdgdgdg.com>
2021-12-13 17:23:00 +0900

    fix: imageのファイルを適切に分割

--- assets/script/contactLinks.js
+++ assets/script/contactLinks.js
@@ -14,12 +14,20 @@ linkData.forEach((value) => {
 
   $("#linkList").insertAdjacentHTML(
     "beforeend",
-    "<li><a href='https://" +
-      formated +
-      ".dgdgdgdg.com' target='_blank' rel='noopener norefferer' class='is-flex'><div class='icon'><img src='/assets/image/i-" +
-      formated +
-      ".webp' alt='icon'></div><span class='sub'>" +
-      value +
-      "</span></a></li>"
+    `
+    <li>
+      <a
+        href='https://${formated}.dgdgdgdg.com'
+        target='_blank'
+        rel='noopener norefferer'
+        class='is-flex'
+      >
+      <div class='icon'>
+        <img src='/assets/image/icon/${formated}.webp'>
+      </div>
+      <span class='sub'>${value}</span>
+      </a>
+    </li>
+    `
   );
 });
