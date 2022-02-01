6abeefbab24cf488b75e0cfc334eccf95175c4b2
dgdgdgdg <ren.y@dgdgdgdg.com>
2022-02-01 18:41:42 +0900

    Merge branch 'develop'

--- assets/script/header.js
+++ assets/script/header.js
@@ -1,15 +1,13 @@
-const writeHeader = (getData) => {
-  const writeText = getData.getElementsByTagName("header")[0].innerHTML;
-  $("#header").innerHTML = writeText;
-};
+const writeHeader = (getData) =>
+  ($("#header").innerHTML = $("header", getData)[0].innerHTML);
 
 const headerUrl = "/assets/common/header.html";
 
 fetch(headerUrl)
   .then((response) => response.text())
-  .then((data) => {
-    writeHeader(new DOMParser().parseFromString(data, "text/html"));
-  });
+  .then((data) =>
+    writeHeader(new DOMParser().parseFromString(data, "text/html"))
+  );
 
 const menuOpen = () =>
   Array.from($(".header-menu")).forEach((value) =>
--- assets/script/main.js
+++ assets/script/main.js
@@ -1,26 +1,12 @@
-const $ = (target) => {
-  const firstChar = target.charAt(0);
-
-  if (firstChar === "#") {
-    target = target.slice(1);
-    const Elm = document.getElementById(`${target}`);
-    return Elm;
-  } else if (firstChar === ".") {
-    target = target.slice(1);
-    const Elm = document.getElementsByClassName(`${target}`);
-
-    if (Elm.length === 0) {
-      return null;
-    } else {
-      return Elm;
-    }
+const $ = (target, doc = document) => {
+  if (target[0] === "#") {
+    const Elm = doc.getElementById(`${target.slice(1)}`);
+    return Elm ? Elm : undefined;
+  } else if (target[0] === ".") {
+    const Elm = doc.getElementsByClassName(`${target.slice(1)}`);
+    return Elm ? Elm : undefined;
   } else {
-    const Elm = document.getElementsByTagName(`${target}`);
-
-    if (Elm.length === 0) {
-      return null;
-    } else {
-      return Elm;
-    }
+    const Elm = doc.getElementsByTagName(`${target}`);
+    return Elm ? Elm : undefined;
   }
 };
