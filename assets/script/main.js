29cdfaf3c454f383196be86b5e1696ec690a04a5
dg4-design <ren.y@dgdgdgdg.com>
2022-02-01 18:26:56 +0900

    fix: js省略記法

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,6 +1,12 @@
-const $ = (target) => {
-  if (target[0] === "#") document.getElementById(`${target.slice(1)}`);
-  else if (target[0] === ".")
-    document.getElementsByClassName(`${target.slice(1)}`);
-  else document.getElementsByTagName(`${target}`);
+const $ = (target, doc = document) => {
+  if (target[0] === "#") {
+    const Elm = doc.getElementById(`${target.slice(1)}`);
+    return Elm ? Elm : undefined;
+  } else if (target[0] === ".") {
+    const Elm = doc.getElementsByClassName(`${target.slice(1)}`);
+    return Elm ? Elm : undefined;
+  } else {
+    const Elm = doc.getElementsByTagName(`${target}`);
+    return Elm ? Elm : undefined;
+  }
 };
