5ccd50ed6160c4fa6e7702ea25307f70ac90bec4
dg4-design <ren.y@dgdgdgdg.com>
2022-02-01 16:45:16 +0900

    fix: js省略記法

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,12 +1,6 @@
 const $ = (target) => {
-  if (target[0] === "#") {
-    const Elm = document.getElementById(`${target.slice(1)}`);
-    return Elm ? Elm : undefined;
-  } else if (target[0] === ".") {
-    const Elm = document.getElementsByClassName(`${target.slice(1)}`);
-    return Elm ? Elm : undefined;
-  } else {
-    const Elm = document.getElementsByTagName(`${target}`);
-    return Elm ? Elm : undefined;
-  }
+  if (target[0] === "#") document.getElementById(`${target.slice(1)}`);
+  else if (target[0] === ".")
+    document.getElementsByClassName(`${target.slice(1)}`);
+  else document.getElementsByTagName(`${target}`);
 };
