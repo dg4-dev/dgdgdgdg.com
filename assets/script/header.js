d393b584c98da5e5059f3520991bcf2623f96f3c
dgdgdgdg <ren.y@dgdgdgdg.com>
2022-03-03 15:34:24 +0900

    fix: headerを最適化

--- assets/script/header.js
+++ assets/script/header.js
@@ -1,5 +1,11 @@
-const writeHeader = (getData) =>
-  ($("#header").innerHTML = $("header", getData)[0].innerHTML);
+const writeHeader = (getData) => {
+  $("header")[0].innerHTML = $("header", getData)[0].innerHTML;
+
+  $("#menuBtn").onclick = () =>
+    Array.from($(".header-menu")).forEach((value) =>
+      value.classList.toggle("is-open")
+    );
+};
 
 const headerUrl = "/assets/common/header.html";
 
@@ -7,9 +13,4 @@ fetch(headerUrl)
   .then((response) => response.text())
   .then((data) =>
     writeHeader(new DOMParser().parseFromString(data, "text/html"))
-  );
-
-const menuOpen = () =>
-  Array.from($(".header-menu")).forEach((value) =>
-    value.classList.toggle("is-open")
   );
