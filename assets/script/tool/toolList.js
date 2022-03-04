40d6379b904cdd9c1ed06481d58d69c6a1ca653f
dg4-design <ren.y@dgdgdgdg.com>
2022-03-05 02:01:09 +0900

    create tool page

---
+++ assets/script/tool/toolList.js
@@ -0,0 +1,103 @@
+const writeTools = (getData) => {
+  getData.forEach((cate) => {
+    const toolCate = document.createElement("section");
+    toolCate.className = "tool-category";
+    $("#toolPage").appendChild(toolCate);
+
+    const toolContain = document.createElement("div");
+    toolContain.className = "container center";
+    toolCate.appendChild(toolContain);
+
+    const cateTitle = document.createElement("h2");
+    cateTitle.className = "center";
+    toolContain.appendChild(cateTitle);
+
+    const titleEn = document.createElement("span");
+    titleEn.className = "en";
+    titleEn.textContent = cate.contentName.en;
+    cateTitle.appendChild(titleEn);
+
+    const titleJa = document.createElement("span");
+    titleJa.className = "ja";
+    titleJa.textContent = cate.contentName.ja;
+    cateTitle.appendChild(titleJa);
+
+    const toolWrapper = document.createElement("div");
+    toolWrapper.className = "tool-wrapper";
+    toolCate.appendChild(toolWrapper);
+
+    const toolList = document.createElement("div");
+    toolList.className = "tool-list center is-flex";
+    toolWrapper.appendChild(toolList);
+
+    cate.items.forEach((cateItem) => {
+      const toolItem = document.createElement("div");
+      toolItem.className = "tool-item";
+      toolList.appendChild(toolItem);
+
+      const toolItemCover = document.createElement("div");
+      toolItemCover.className = "cover click";
+      toolItem.appendChild(toolItemCover);
+
+      const coverImage = document.createElement("img");
+      const fmtMaker = cateItem.maker.toLowerCase().replace(/[." -]/g, "");
+      const fmtName = cateItem.name.toLowerCase().replace(/[." -]/g, "");
+      coverImage.src = `/assets/image/tool/${fmtMaker}-${fmtName}.png`;
+      toolItemCover.appendChild(coverImage);
+
+      const toolItemModal = document.createElement("div");
+      toolItemModal.className = "modal";
+      toolItem.appendChild(toolItemModal);
+
+      const modalCloseArea = document.createElement("div");
+      modalCloseArea.className = "close-area click";
+      toolItemModal.appendChild(modalCloseArea);
+
+      const modalCard = document.createElement("div");
+      modalCard.className = "card is-flex";
+      toolItemModal.appendChild(modalCard);
+
+      const cardImage = document.createElement("div");
+      cardImage.className = "img";
+      modalCard.appendChild(cardImage);
+
+      const cardImageInner = document.createElement("img");
+      cardImageInner.src = coverImage.src;
+      cardImage.appendChild(cardImageInner);
+
+      modalCard.insertAdjacentHTML(
+        "beforeend",
+        `<div class="desc">
+          <div class="name">${cateItem.name}</div>
+          <div class="maker-and-gen">${cateItem.maker}　${cateItem.gen}</div>
+          <div class="about">${cateItem.about}</div>
+        </div>`
+      );
+
+      const modalCloseBtn = document.createElement("div");
+      modalCloseBtn.className = "close-btn click";
+      modalCard.appendChild(modalCloseBtn);
+
+      toolItemCover.addEventListener("click", () => {
+        toolItemModal.classList.toggle("active");
+        document.body.classList.toggle("is-open");
+      });
+
+      modalCloseArea.addEventListener("click", () => {
+        toolItemModal.classList.toggle("active");
+        document.body.classList.toggle("is-open");
+      });
+
+      modalCloseBtn.addEventListener("click", () => {
+        toolItemModal.classList.toggle("active");
+        document.body.classList.toggle("is-open");
+      });
+    });
+  });
+};
+
+const toolDataURL = "/assets/script/tool/toolData.json";
+
+fetch(toolDataURL)
+  .then((response) => response.json())
+  .then((data) => writeTools(data));
