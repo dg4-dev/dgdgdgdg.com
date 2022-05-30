f7ebb287a70cc6721eddc14795152cf913c215ce
dgdgdgdg / だがし <ren.y@dgdgdgdg.com>
2022-05-30 18:39:01 +0900

    update: fv 💫

--- assets/script/getInsta.js
+++ assets/script/getInsta.js
@@ -1,5 +1,6 @@
 const writeInsta = (getData) => {
   getData.media.data.forEach(({ permalink, media_url }) => {
+    console.log(permalink, media_url);
     const instaPost = c$();
     instaPost.className = "post-photo";
     g$("#postList").appendChild(instaPost);
@@ -18,7 +19,7 @@ const writeInsta = (getData) => {
 
 const getInstaCount = 21;
 
-const instaUrl = `https://graph.facebook.com/v3.0/17841445773642608?fields=name%2Cmedia.limit(${getInstaCount})%7Bcaption%2Clike_count%2Cmedia_url%2Cpermalink%2Ctimestamp%2Cusername%7D&access_token=EABFZByCZAxiGABAIRfTq7Ed6i1aSv29Ybhfa9gZCa5eDy7OQlMDjyYKznYicaChlUJMRmD3Lb7hOFDRhsv0pryvZBTHZAjZAUUZCZC1Xgbb20mJIHpge1Gvp7P0I7kwvqZCt0xyfvwwGjVuJ7Y1ZAfyZAWgUf0Xg9ZAcNaZBhVUtAExWYp2LqkZCPrlYAV`;
+const instaUrl = `https://graph.facebook.com/v3.0/17841445773642608?fields=name%2Cmedia.limit(${getInstaCount})%7Bmedia_url%2Cpermalink%7D&access_token=EABFZByCZAxiGABAIRfTq7Ed6i1aSv29Ybhfa9gZCa5eDy7OQlMDjyYKznYicaChlUJMRmD3Lb7hOFDRhsv0pryvZBTHZAjZAUUZCZC1Xgbb20mJIHpge1Gvp7P0I7kwvqZCt0xyfvwwGjVuJ7Y1ZAfyZAWgUf0Xg9ZAcNaZBhVUtAExWYp2LqkZCPrlYAV`;
 
 fetch(instaUrl)
   .then((response) => response.json())
