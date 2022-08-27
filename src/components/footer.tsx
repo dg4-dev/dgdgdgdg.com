221516f849471a3fae631d409d164b76e19d1c1f
dgdgdgdg <ren.y@dgdgdgdg.com>
2022-08-28 05:06:21 +0900

    update: footerできた！privacyもつくった

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,248 @@
+import { css } from "@emotion/react";
+import Link from "next/link";
+
 import type { NextPage } from "next";
 
+import Container from "@/components/container";
+import common from "@/styles/common";
+import bp from "@/styles/config/breakpoint";
+import dg4Color from "@/styles/config/color";
+
+const footerStyle = css`
+  background-color: ${dg4Color.black};
+
+  padding-top: 30px;
+  padding-bottom: 90px;
+
+  ${bp.tab} {
+    padding-top: 20px;
+    padding-bottom: 70px;
+  }
+
+  .container {
+    display: flex;
+    max-width: 1024px;
+    width: 100%;
+    justify-content: space-between;
+    padding: 0 40px;
+
+    ${bp.tab} {
+      padding: 0 20px;
+      justify-content: flex-end;
+    }
+
+    ${bp.sp} {
+      padding: 0 12px;
+    }
+  }
+`;
+
+const pageList = css`
+  gap: 0 40px;
+
+  ${bp.tab} {
+    display: none;
+  }
+
+  li {
+    flex-direction: column;
+  }
+  li a {
+    display: block;
+    font-size: 16px;
+    font-weight: bold;
+    color: #fff;
+
+    padding: 10px 0;
+    transition: all 0.3s;
+  }
+
+  li a.child-page::before {
+    content: "└ ";
+  }
+
+  li a:hover {
+    opacity: 0.5;
+    transition: all 0.3s;
+  }
+
+  li a.page-link {
+    font-family: "Dont", sans-serif;
+    font-size: 24px;
+    font-weight: normal;
+    opacity: 0.6;
+    transition: all 0.3s;
+  }
+
+  li a.page-link:hover {
+    opacity: 0.1;
+    transition: all 0.3s;
+  }
+`;
+
+const credit = css`
+  flex-direction: column;
+  align-items: flex-end;
+  margin-top: 10px;
+`;
+
+const iconWrapper = css`
+  align-items: center;
+  gap: 8px;
+  margin-bottom: 16px;
+`;
+
+const text = css`
+  display: block;
+  color: ${dg4Color.cyan};
+  font-family: "Dont", sans-serif;
+  font-size: 16px;
+  margin-right: 8px;
+`;
+
+const iconItem = css`
+  a {
+    display: block;
+    width: 40px;
+    height: 40px;
+    padding: 8px;
+  }
+
+  a:hover {
+    opacity: 0.5;
+  }
+
+  a object {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const linkPrivacy = css`
+  text-decoration: underline;
+  display: block;
+  margin-bottom: 12px;
+
+  :hover {
+    opacity: 0.5;
+  }
+`;
+
+const whiteText = css`
+  font-size: 12px;
+  color: #fff;
+`;
+
 const Footer: NextPage = () => {
-  return <footer>&copy; 2021 dgdgdgdg</footer>;
+  return (
+    <footer css={footerStyle}>
+      <Container>
+        <ul css={[pageList, common.flex]}>
+          <li css={common.flex}>
+            <Link href="/about/">
+              <a className="page-link">About</a>
+            </Link>
+            <ul>
+              <li>
+                <Link href="/about/#overview">概要</Link>
+              </li>
+              <li>
+                <Link href="/about/brand/">
+                  <a className="child-page">ブランドリソース</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/about/#skill">スキル</Link>
+              </li>
+              <li>
+                <Link href="/about/#tool">ツール</Link>
+              </li>
+              <li>
+                <Link href="/about/tool/">
+                  <a className="child-page">ツール一覧</a>
+                </Link>
+              </li>
+            </ul>
+          </li>
+          <li css={common.flex}>
+            <Link href="/works/">
+              <a className="page-link">Works</a>
+            </Link>
+            <ul>
+              <li>
+                <Link href="/works/#font">フォント</Link>
+              </li>
+              <li>
+                <Link href="/works/#logo">ロゴ</Link>
+              </li>
+              <li>
+                <Link href="/works/#photo">写真</Link>
+              </li>
+              <li>
+                <Link href="/works/#video">動画</Link>
+              </li>
+              <li>
+                <Link href="/works/#web">Webサイト</Link>
+              </li>
+            </ul>
+          </li>
+          <li css={common.flex}>
+            <Link href="/service/">
+              <a className="page-link">Service</a>
+            </Link>
+            <ul>
+              <li>
+                <Link href="/service/#flow">制作の流れ</Link>
+              </li>
+              <li>
+                <Link href="/service/#plan">プラン</Link>
+              </li>
+            </ul>
+          </li>
+          <li css={common.flex}>
+            <Link href="/contact/">
+              <a className="page-link">Contact</a>
+            </Link>
+            <ul>
+              <li>
+                <Link href="/contact/#message">メッセージ</Link>
+              </li>
+              <li>
+                <Link href="/contact/#link">リンク</Link>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <div css={[credit, common.flex]}>
+          <ul css={[iconWrapper, common.flex]}>
+            <p css={text}>Share on</p>
+            <li css={iconItem}>
+              <a
+                href="https://twitter.com/intent/tweet?text=%0a%23dg4_design%20@dg4_design%20より%0ahttps://dgdgdgdg.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <object data="/images/icon-twitter.svg" type="image/svg+xml" />
+              </a>
+            </li>
+            <li css={iconItem}>
+              <a
+                href="http://www.facebook.com/share.php?u=https://dgdgdgdg.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <object data="/images/icon-facebook.svg" type="image/svg+xml" />
+              </a>
+            </li>
+          </ul>
+          <Link href="/privacy-policy/">
+            <a css={[linkPrivacy, whiteText]}>プライバシーポリシー</a>
+          </Link>
+          <p css={whiteText}>©︎ 2021 dgdgdgdg</p>
+        </div>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
