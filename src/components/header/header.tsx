acf312f894f48602669d180bea0cb55325ab64f2
dgdgdgdg <ren.y@dgdgdgdg.com>
2022-08-28 02:55:36 +0900

    update: headerのoutsideできた

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,23 +1,172 @@
 import { css } from "@emotion/react";
+import Link from "next/link";
+import { useState } from "react";
 
 import type { NextPage } from "next";
 
-import Button from "@/components/button";
 import HeaderLink from "@/components/header/headerLink";
+import common from "@/styles/common";
+import bp from "@/styles/config/breakpoint";
 import dg4Color from "@/styles/config/color";
+
+const outside = css`
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+
+  padding-left: 26px;
+  z-index: 1000;
+
+  ${bp.sp} {
+    padding-left: 12px;
+  }
+`;
+
+const logo = css`
+  width: 150px;
+  height: auto;
+
+  ${bp.sp} {
+    width: 120px;
+  }
+`;
+
+const burger = css`
+  width: 90px;
+  height: 90px;
+  background-color: ${dg4Color.cyan};
+
+  justify-content: center;
+  align-items: center;
+
+  ${bp.sp} {
+    width: 65px;
+    height: 65px;
+  }
+
+  :hover .line-item1 {
+    top: 6px;
+    transition: all 0.2s;
+
+    ${bp.sp} {
+      top: 4px;
+    }
+  }
+  :hover .line-item2 {
+    visibility: hidden;
+    opacity: 0;
+    transition: all 0.2s;
+  }
+
+  :hover .line-item3 {
+    top: 18px;
+    transition: all 0.2s;
+    ${bp.sp} {
+      top: 12px;
+    }
+  }
+`;
+
+const line = css`
+  width: 35px;
+  height: 27px;
+
+  position: relative;
+
+  ${bp.sp} {
+    width: 30px;
+    height: 19px;
+  }
+
+  .line-item {
+    width: 100%;
+    height: 3px;
+
+    position: absolute;
+
+    background-color: #fff;
+  }
+
+  .line-item1 {
+    top: 0;
+    transition: all 0.2s;
+  }
+
+  .line-item1.is-open {
+    top: 12px;
+    transition: all 0.2s;
+
+    ${bp.sp} {
+      top: 8px;
+    }
+  }
+
+  .line-item2 {
+    visibility: visible;
+    opacity: 1;
+    top: 12px;
+    transition: all 0.2s;
+
+    ${bp.sp} {
+      top: 8px;
+    }
+  }
+
+  .line-item2.is-open {
+    visibility: hidden;
+    opacity: 0;
+    transition: all 0.2s;
+  }
+
+  .line-item3 {
+    top: 24px;
+    transition: all 0.2s;
+    ${bp.sp} {
+      top: 16px;
+    }
+  }
+
+  .line-item3.is-open {
+    top: 12px;
+    transition: all 0.2s;
+
+    ${bp.sp} {
+      top: 8px;
+    }
+  }
+`;
 
 const nav = css`
   background-color: ${dg4Color.cyan};
 `;
 
 const Header: NextPage = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuFunction = () => {
+    setOpenMenu(!openMenu);
+  };
+  let openState = openMenu ? "is-open" : "";
+
   return (
-    <header>
+    <header css={common.flex}>
+      <div css={[outside, common.flex]}>
+        <Link href="/">
+          <a css={logo}>
+            <object data="/images/logo.svg" type="image/svg+xml" />
+          </a>
+        </Link>
+        <div css={[burger, common.click, common.flex]} onClick={() => menuFunction()}>
+          <div css={[line, common.flex]}>
+            <div className={`line-item line-item1 ${openState}`} />
+            <div className={`line-item line-item2 ${openState}`} />
+            <div className={`line-item line-item3 ${openState}`} />
+          </div>
+        </div>
+      </div>
+
       <nav css={nav}>
         <ul>
-          <li>
-            <Button href="/" text="Top" />
-          </li>
           <li>
             <HeaderLink href="/about" en="About" ja="dgdgdgdgとは" />
             <ul>
--- src/styles/common.ts
+++ src/styles/common.ts
@@ -1,15 +1,18 @@
 import { css } from "@emotion/react";
 
-const common: {
-  flex: any;
-  center: any;
-} = {
+const common: any = {
   flex: css`
     display: flex;
   `,
   center: css`
     margin-left: auto;
     margin-right: auto;
+  `,
+  click: css`
+    cursor: -webkit-image-set(url("/images/cursor-pointer.png") 1x, url("/images/cursor-pointer@2x.png") 2x), pointer;
+    * {
+      cursor: -webkit-image-set(url("/images/cursor-pointer.png") 1x, url("/images/cursor-pointer@2x.png") 2x), pointer;
+    }
   `,
 };
 
