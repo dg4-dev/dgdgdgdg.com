c58ec2e568d763d042db158dc67d97a0152caad6
dgdgdgdg / だがし <ren.y@dgdgdgdg.com>
2022-08-30 02:08:38 +0900

    fix: [key in string]おぼえた　つっよ

--- src/styles/config.ts
+++ src/styles/config.ts
@@ -1,8 +1,5 @@
 export const dg4Color: {
-  cyan: string;
-  black: string;
-  blue: string;
-  orange: string;
+  [key in string]: string;
 } = {
   cyan: "#00d4d4",
   black: "#444444",
@@ -10,20 +7,19 @@ export const dg4Color: {
   orange: "#d44d00",
 };
 
-export const cWidth: {
-  pc: string;
-  tab: string;
-  sp: string;
-} = {
+type device = {
+  sp?: string;
+  tab?: string;
+  pc?: string;
+};
+
+export const cWidth: device = {
   pc: "800px",
   tab: "calc(100% - (20px * 2))",
   sp: "calc(100% - (12px * 2))",
 };
 
-export const bp: {
-  tab: string;
-  sp: string;
-} = {
+export const bp: device = {
   tab: "@media (max-width: 1024px)",
   sp: "@media (max-width: 640px)",
 };
