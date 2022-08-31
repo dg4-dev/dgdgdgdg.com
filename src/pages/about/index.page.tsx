0fa235c71c9ad6f1f9c95c13a4efbbf653c41881
dgdgdgdg <ren.y@dgdgdgdg.com>
2022-08-31 15:30:20 +0900

    fix: ちょっと多いな　関数コンポーネントに分けるか

--- src/pages/about/index.page.tsx
+++ src/pages/about/index.page.tsx
@@ -1,10 +1,11 @@
 import { css } from "@emotion/react";
-import dayjs from "dayjs";
 import Image from "next/image";
 
 import type { NextPage } from "next";
 
 import Age from "@/api/nowAge";
+import meaningItemElm from "@/components/about/meaningItem";
+import skillGraph from "@/components/about/skillGraph";
 import Button from "@/components/button";
 import Container from "@/components/container";
 import H1 from "@/components/heading/h1";
@@ -12,7 +13,7 @@ import H2 from "@/components/heading/h2";
 import H3 from "@/components/heading/h3";
 import Layout from "@/components/layout";
 import { center, flex } from "@/styles/common";
-import { bp, dg4Color } from "@/styles/config";
+import { bp } from "@/styles/config";
 
 const containerContent = css`
   flex-direction: column;
@@ -93,175 +94,6 @@ const meaningItem = css`
 const skillContent = css`
   width: fit-content;
 `;
-
-const meaningRaw: {
-  phrase: string;
-  mean: [string, ...string[]];
-}[] = [
-  {
-    phrase: "4",
-    mean: ["四季", "四つ葉のクローバー", "四次元", ".mp4"],
-  },
-  {
-    phrase: "dgdg",
-    mean: ["ドラムの音"],
-  },
-  {
-    phrase: "deep",
-    mean: ["深い", "奥行きのある"],
-  },
-  {
-    phrase: "駄菓子",
-    mean: ["多種多様", "多方面"],
-  },
-  {
-    phrase: "dig",
-    mean: ["調べる", "いいものを発掘する"],
-  },
-  {
-    phrase: "digital",
-    mean: ["デジタル"],
-  },
-  {
-    phrase: "delight",
-    mean: ["喜び", "楽しみ"],
-  },
-  {
-    phrase: "design",
-    mean: ["デザイン"],
-  },
-  { phrase: "geek", mean: ["オタク"] },
-];
-
-const meaningItemElm = meaningRaw.map((value) => {
-  const meaningBit = css`
-    width: fit-content;
-    padding: 0 24px;
-  `;
-
-  const meaningPhrase = css`
-    white-space: nowrap;
-    font-family: "Dont", sans-serif;
-    font-size: 64px;
-    margin-bottom: 16px;
-
-    ${bp.sp} {
-      font-size: 56px;
-    }
-  `;
-
-  const cyan = css`
-    color: ${dg4Color.cyan};
-  `;
-
-  const noto = css`
-    font-family: "Zen Kaku Gothic New", sans-serif;
-    font-weight: bold;
-  `;
-
-  const meaningPara = css`
-    white-space: nowrap;
-    line-height: 24px;
-  `;
-
-  const meaningPhraseElms = value.phrase.split("").map((letter, index) => {
-    if (letter.match(/[dg4]/g)) {
-      return (
-        <span key={`${letter}${index}`} css={cyan}>
-          {letter}
-        </span>
-      );
-    } else if (letter.match(/[駄菓子]/g)) {
-      return (
-        <span key={`${letter}${index}`} css={[cyan, noto]}>
-          {letter}
-        </span>
-      );
-    } else {
-      return <span key={`${letter}${index}`}>{letter}</span>;
-    }
-  });
-
-  const meaningParaElm = value.mean.map((meanParaItem) => (
-    <>
-      {meanParaItem}
-      <br />
-    </>
-  ));
-
-  return (
-    <li key={value.phrase} css={meaningBit}>
-      <div css={meaningPhrase}>{meaningPhraseElms}</div>
-      <p css={meaningPara}>{meaningParaElm}</p>
-    </li>
-  );
-});
-
-const skillRaw = [
-  {
-    name: "デザイン",
-    start: dayjs("2015-05-01"),
-  },
-  {
-    name: "映像制作",
-    start: dayjs("2017-04-01"),
-  },
-  {
-    name: "モーショングラフィックス",
-    start: dayjs("2019-04-01"),
-  },
-  {
-    name: "Web制作",
-    start: dayjs("2019-10-01"),
-  },
-];
-
-const skillGraph = skillRaw.map(({ name, start }) => {
-  const skillTime = Number(dayjs()) - Number(start);
-  const firstTime = Number(dayjs()) - Number(skillRaw[0].start);
-  const round = ~~((skillTime / firstTime) * 100);
-
-  const skillItem = css`
-    :not(:first-of-type) {
-      margin-top: 40px;
-    }
-  `;
-
-  const skillName = css`
-    font-weight: bold;
-    margin-bottom: 10px;
-  `;
-
-  const skillBar = css`
-    width: 350px;
-    height: 10px;
-
-    background-color: #eee;
-
-    position: relative;
-
-    ${bp.sp} {
-      width: 100%;
-    }
-
-    ::after {
-      content: "";
-      display: block;
-
-      height: 100%;
-      background-color: ${dg4Color.cyan};
-      position: absolute;
-      width: ${round * 0.95}%;
-    }
-  `;
-
-  return (
-    <li key={name} css={skillItem}>
-      <p css={skillName}>{name}</p>
-      <div css={skillBar}></div>
-    </li>
-  );
-});
 
 const About: NextPage = () => {
   return (
