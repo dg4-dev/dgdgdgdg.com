1717488ac089883b872db385d5ad500d5735f6d9
dgdgdgdg / だがし <ren.y@dgdgdgdg.com>
2022-09-02 00:31:35 +0900

    update: brandページできた！

--- src/components/note.tsx
+++ src/components/note.tsx
@@ -1,5 +1,7 @@
 import { css } from "@emotion/react";
 
+import { dg4Color } from "@/styles/config";
+
 type Props = {
   className?: string;
   children: React.ReactNode;
@@ -9,6 +11,21 @@ const para = css`
   line-height: 1.5;
   text-align: center;
   margin-bottom: 32px;
+
+  a {
+    word-break: break-all;
+    font-size: 16px;
+    color: ${dg4Color.cyan};
+    text-decoration: underline;
+    transition: all 0.3s;
+  }
+  a:hover {
+    opacity: 0.5;
+  }
+
+  span {
+    font-weight: bold;
+  }
 `;
 
 const Note = ({ className, children }: Props) => {
