626eea51138b00c4b6fc448786a854ec74004fd8
dgdgdgdg <ren.y@dgdgdgdg.com>
2023-02-28 17:54:55 +0900

    fix: equipmentにソートつけた

--- src/pages/api/equipments/categoryNames.ts
+++ src/pages/api/equipments/categoryNames.ts
@@ -2,51 +2,63 @@ export type categoryNamesProps =
   | {
       en: "?";
       ja: "?";
+      order: 10;
     }
   | {
       en: "Viewing Content";
       ja: "端末機器";
+      order: 20;
     }
   | {
       en: "Taking Shots";
       ja: "撮影する";
+      order: 30;
     }
   | {
       en: "Develop and Design";
       ja: "開発とデザイン";
+      order: 40;
     }
   | {
       en: "Audio";
       ja: "音響処理";
+      order: 50;
     }
   | {
       en: "Music Performance";
       ja: "演奏する";
+      order: 60;
     };
 
 export const categoryNames: categoryNamesProps[] = [
   {
     en: "?",
     ja: "?",
+    order: 10,
   },
   {
     en: "Viewing Content",
     ja: "端末機器",
+    order: 20,
   },
   {
     en: "Taking Shots",
     ja: "撮影する",
+    order: 30,
   },
   {
     en: "Develop and Design",
     ja: "開発とデザイン",
+    order: 40,
   },
   {
     en: "Audio",
     ja: "音響処理",
+    order: 50,
   },
   {
     en: "Music Performance",
     ja: "演奏する",
+    order: 60,
   },
 ];
