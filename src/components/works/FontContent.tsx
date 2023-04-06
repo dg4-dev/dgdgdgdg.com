0e4a0076ab686e0a3f163c2d601178aa0cc6ca2e
dgdgdgdg <ren.y@dgdgdgdg.com>
2023-04-06 19:04:09 +0900

    fix: 0パディング入れました！！

--- src/components/works/FontContent.tsx
+++ src/components/works/FontContent.tsx
@@ -61,7 +61,11 @@ const FontContent = ({ name, text }: { name: "Dont" | "Dont Round" | "Dont Circl
     };
 
     const randomColor = () => {
-      return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+      // 0パディング
+      return `#${Math.floor(Math.random() * 16777216)
+        .toString(16)
+        .padStart(6, "0")}`;
+      // return `#${Math.floor(Math.random() * 16777216).toString(16)}`;
     };
 
     setFontSizeValue(randomNumber(170) + 10);
