cab6c47df5f0509173fa8d87046a2eb85197e662
dgdgdgdg <ren.y@dgdgdgdg.com>
2023-08-02 22:30:41 +0900

    change domain

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -144,7 +144,7 @@ const LinkItems = () => {
     }
   `;
 
-  const linkData = ["zenn", "instagram", "youtube", "suzuri", "github", "misskey", "twitter", "note"];
+  const linkData = ["zenn", "instagram", "youtube", "suzuri", "github", "misskey", "x", "note"];
 
   const linkElm = linkData.map((value) => {
     const href = `https://${value}.dgdgdgdg.com`;
