cff68d9f4b51a9fb50b6779936ff6d78aa8e5d1f
dgdgdgdg / だがし <ren.y@dgdgdgdg.com>
2023-08-08 16:48:02 +0900

    add parallax

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -3,10 +3,10 @@ import { css } from "@emotion/react";
 import type { NextPage } from "next";
 
 import Layout from "@/components/Layout";
+import Parallax from "@/components/Parallax";
 import { breakPoint, dg4Color } from "@/styles/config";
 
 // TODO: Lottieのためのリンク
-// https://blue-de.com/butterfly-scissor-difference/
 // https://codepen.io/michaelzero00/pen/KKXJjOg
 // https://zenn.dev/hndr/scraps/b5b33f1157db5d#comment-292449cb553aae
 
@@ -23,7 +23,7 @@ const fv = css`
     margin-bottom: 0;
   }
 
-  .space {
+  #space {
     width: 97vw;
 
     position: absolute;
@@ -33,12 +33,17 @@ const fv = css`
 
     margin: auto;
 
+    transform: translate3d(0, 0, 0);
+    will-change: transform;
+
     ${breakPoint.sp} {
       width: unset;
-      height: 80vh;
+      height: 100vh;
 
       left: 0;
       right: 0;
+
+      margin-left: -20vw;
     }
   }
 
@@ -46,28 +51,28 @@ const fv = css`
     position: absolute;
   }
 
-  .logo:nth-of-type(2) {
+  #logo1 {
     width: 10vw;
 
     top: 32vh;
     left: 30vw;
   }
 
-  .logo:nth-of-type(3) {
+  #logo2 {
     width: 14vw;
 
     top: 18vh;
     left: 51vw;
   }
 
-  .logo:nth-of-type(4) {
+  #logo3 {
     width: 52vw;
 
     top: 44vh;
     left: 54vw;
   }
 
-  .logo:nth-of-type(5) {
+  #logo4 {
     width: 138vw;
 
     top: 58vh;
@@ -79,11 +84,19 @@ const Home: NextPage = () => {
   return (
     <Layout description="2021年2月1日に開業。デザイン、映像制作、Web制作をメインに、多方面であることを生かしつつ洗練されたものを目指して制作していきます。">
       <section css={fv}>
-        <object className="space" data="/images/fv/space.svg" type="image/svg+xml"></object>
-        <object className="logo" data="/images/dist/logo-white.svg" type="image/svg+xml"></object>
-        <object className="logo" data="/images/dist/logo-white.svg" type="image/svg+xml"></object>
-        <object className="logo" data="/images/dist/logo-white.svg" type="image/svg+xml"></object>
-        <object className="logo" data="/images/dist/logo-white.svg" type="image/svg+xml"></object>
+        <object id="space" data="/images/fv/space.svg" type="image/svg+xml"></object>
+        <Parallax factor={0.1}>
+          <object className="logo" id="logo1" data="/images/dist/logo-white.svg" type="image/svg+xml"></object>
+        </Parallax>
+        <Parallax factor={0.13}>
+          <object className="logo" id="logo2" data="/images/dist/logo-white.svg" type="image/svg+xml"></object>
+        </Parallax>
+        <Parallax factor={0.2}>
+          <object className="logo" id="logo3" data="/images/dist/logo-white.svg" type="image/svg+xml"></object>
+        </Parallax>
+        <Parallax factor={0.4}>
+          <object className="logo" id="logo4" data="/images/dist/logo-white.svg" type="image/svg+xml"></object>
+        </Parallax>
       </section>
     </Layout>
   );
