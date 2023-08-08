145b67b3478797074c0e36d9a3136ce566e5b7ff
dgdgdgdg / だがし <ren.y@dgdgdgdg.com>
2023-08-08 15:50:49 +0900

    to svg

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,9 +1,9 @@
 import { css } from "@emotion/react";
-import Image from "next/image";
 
 import type { NextPage } from "next";
 
 import Layout from "@/components/Layout";
+import { breakPoint, dg4Color } from "@/styles/config";
 
 // TODO: Lottieのためのリンク
 // https://blue-de.com/butterfly-scissor-difference/
@@ -11,9 +11,11 @@ import Layout from "@/components/Layout";
 // https://zenn.dev/hndr/scraps/b5b33f1157db5d#comment-292449cb553aae
 
 const fv = css`
+  background-color: ${dg4Color.black};
+
   position: relative;
   width: 100vw;
-  height: 100vh;
+  height: 120vh;
 
   padding-top: 0;
 
@@ -21,8 +23,55 @@ const fv = css`
     margin-bottom: 0;
   }
 
-  span img {
-    object-fit: cover;
+  .space {
+    width: 97vw;
+
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: -14vw;
+
+    margin: auto;
+
+    ${breakPoint.sp} {
+      width: unset;
+      height: 80vh;
+
+      left: 0;
+      right: 0;
+    }
+  }
+
+  .logo {
+    position: absolute;
+  }
+
+  .logo:nth-of-type(2) {
+    width: 10vw;
+
+    top: 32vh;
+    left: 30vw;
+  }
+
+  .logo:nth-of-type(3) {
+    width: 14vw;
+
+    top: 18vh;
+    left: 51vw;
+  }
+
+  .logo:nth-of-type(4) {
+    width: 52vw;
+
+    top: 44vh;
+    left: 54vw;
+  }
+
+  .logo:nth-of-type(5) {
+    width: 138vw;
+
+    top: 58vh;
+    left: -77vw;
   }
 `;
 
@@ -30,7 +79,11 @@ const Home: NextPage = () => {
   return (
     <Layout description="2021年2月1日に開業。デザイン、映像制作、Web制作をメインに、多方面であることを生かしつつ洗練されたものを目指して制作していきます。">
       <section css={fv}>
-        <Image priority={true} src="/images/fv/top.png" layout="fill" alt="dg4 first view" />
+        <object className="space" data="/images/fv/space.svg" type="image/svg+xml"></object>
+        <object className="logo" data="/images/dist/logo-white.svg" type="image/svg+xml"></object>
+        <object className="logo" data="/images/dist/logo-white.svg" type="image/svg+xml"></object>
+        <object className="logo" data="/images/dist/logo-white.svg" type="image/svg+xml"></object>
+        <object className="logo" data="/images/dist/logo-white.svg" type="image/svg+xml"></object>
       </section>
     </Layout>
   );
